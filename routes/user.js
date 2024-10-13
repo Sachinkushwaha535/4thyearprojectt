3caf9d776e6f3f688178b0b8a552a307673eef47
Sachin Kushwah <sachinkushwah535@gmail.com>
2024-10-13 17:44:49 +0530

    add on in login

--- routes/user.js
+++ routes/user.js
@@ -3,20 +3,31 @@ const router = express.Router();
 const User = require("../models/user");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
-const userController = require("../controller/user.js");
-
+const userController = require("../controller/user");
 
 router.route("/signup")
-.get( userController.Getsignup)
-.post( userController.Postsignup);
-
+    .get((req, res) => {
+        console.log("GET /signup");
+        userController.Getsignup(req, res);
+    })
+    .post((req, res) => {
+        console.log("POST /signup");
+        userController.Postsignup(req, res);
+    });
 
 router.route("/login")
-.get( userController.Getlogin)
-.post( saveRedirectUrl, passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-}), userController.Postlogin);
+    .get((req, res) => {
+        console.log("GET /login");
+        userController.Getlogin(req, res);
+    })
+    .post(saveRedirectUrl, passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true,
+    }), (req, res) => {
+        console.log("POST /login");
+        userController.Postlogin(req, res);
+    });
+
 
 
 
